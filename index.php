ef1101d76d031c3761efb3bb2411c37c7dd687ed
demamano <demaamano@gmail.com>
2022-06-24 13:45:43 +0300

    home page for ethipopian national army website

---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php include("header.php"); ?>   
+    <?php include("navigation.php"); ?>   
+
+    <section class="banner" id="banner">
+        <div class="content">
+            <h2>PRIDE OF THE NATION </h2>
+
+        </div>
+
+    </section>
+    <section class="army-motto">
+        <div class="container">
+            <div class="row">
+                <div class="equality">
+                    <div class="first">
+                        <img src="./images/download.jpeg" style="border-width: 3x;  height: 290px; " alt="">
+                        <h3>EQUALITY</h3>
+                        <p>preserving equality and justice</p>
+                    </div>
+                </div>
+                <div class="border">
+                    <div class="second">
+                        <img src="./images/army.jpeg" alt="">
+                        <H3>BORDER</H3>
+                        <p>securing border from enemy and bandits</p>
+                    </div>
+                </div>
+                <div class="service">
+                    <div class="third">
+                        <img src="./images/national_guard.jpeg" alt="">
+                        <h3>SERVICE</h3>
+                        <p>serving people in need </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <div class="cover"></div>
+
+
+    <div class="content-details">
+        <div class="container-ahead">
+            <div class="row-ahead">
+                <div class="col1">
+                    <div class="r1">
+                        <h3>News</h3>
+                        <div class="hidden-linked">
+                            <h5> <span> <i>15/06/2022</i></span> <a href=""></a> Lorem ipsum dolor sit, amet consectetur
+                                adipisicing elit. Magni, sint!...</h5>
+                            <h5> <span> <i>15/06/2022</i></span><a href=""></a> Lorem ipsum dolor sit amet, consectetur
+                                adipisicing elit. Nihil, asperiores!</h5>
+                            <h5><span> <i>15/06/2022</i></span><a href=""></a> Lorem ipsum dolor sit amet consectetur,
+                                adipisicing elit. Eveniet, doloremque.</h5>
+                            <h5><span> <i>15/06/2022</i></span> <a href=""></a> Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Temporibus, debitis!</h5>
+                            <h5><span> <i>15/06/2022</i></span><a href=""></a> Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit. Maiores, laborum?</h5>
+                            <h5><span> <i>15/06/2022</i></span><a href=""></a> Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. In, culpa.</h5>
+                            <h5><span> <i>15/06/2022</i></span><a href=""></a> Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Numquam, quia.</h5>
+                        </div>
+                    </div>
+                    <div class="r2">
+                        <h3>Military Spokesman</h3>
+                        <div class="news1">
+                            <div class="news1-item">
+                                <div class="news1-image">
+                                    <a href="#"><img src="./images/emblem.png" style="height: 200px;" alt=""></a>
+                                </div>
+                                <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum numquam tenetur, sequi
+                                    minus sapiente assumenda?</h4>
+                                <span class="date">
+                                    <i class="oclock">
+                                        03/10/2021
+                                    </i>
+
+                                </span>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, deleniti.</p>
+                                <div class="more">
+                                    <a href="#">
+                                        <span>READ MORE</span>
+                                    </a>
+                                </div>
+
+                            </div>
+
+                        </div>
+                        <div class="news2">
+                            <div class="news2-item">
+                                <div class="news2-image">
+                                    <a href="#"><img src="./images/emblem.png" style="height: 200px;" alt=""></a>
+                                </div>
+                                <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum numquam tenetur, sequi
+                                    minus sapiente assumenda?</h4>
+                                <span class="date">
+                                    <i class="oclock">
+                                        03/10/2021
+                                    </i>
+
+                                </span>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, deleniti.</p>
+                                <div class="more">
+                                    <a href="#">
+                                        <span>READ MORE</span>
+                                    </a>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="r3">
+                        <div class="news1">
+                            <div class="news1-item">
+                                <div class="news1-image">
+                                    <a href="#"><img src="./images/emblem.png" style="height: 200px;" alt=""></a>
+                                </div>
+                                <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum numquam tenetur, sequi
+                                    minus sapiente assumenda?</h4>
+                                <span class="date">
+                                    <i class="oclock">
+                                        03/10/2021
+                                    </i>
+
+                                </span>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, deleniti.</p>
+                                <div class="more">
+                                    <a href="#">
+                                        <span>READ MORE</span>
+                                    </a>
+                                </div>
+
+                            </div>
+
+                        </div>
+                        <div class="news2">
+                            <div class="news2-item">
+                                <div class="news2-image">
+                                    <a href="#"><img src="./images/emblem.png" style="height: 200px;" alt=""></a>
+                                </div>
+                                <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum numquam tenetur, sequi
+                                    minus sapiente assumenda?</h4>
+                                <span class="date">
+                                    <i class="oclock">
+                                        03/10/2021
+                                    </i>
+
+                                </span>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, deleniti.</p>
+                                <div class="more">
+                                    <a href="#">
+                                        <span>READ MORE</span>
+                                    </a>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <a class="see-more" href="#"> See More</a>
+                    <div class="r4">
+                        <h3>National Projects</h3>
+                        <div class="project1">
+                            <div class="image1">
+                                <img src="./images/home.jpeg" alt="">
+                                Housing
+                            </div>
+                            <div class="image2">
+                                <img src="./images/health.JPG" alt="">
+                                Health Care
+                            </div>
+                            <div class="image3">
+                                <img src="./images/bridg.jpg" alt="">
+                                Bridge and Tunnels
+
+                            </div>
+                        </div>
+                        <div class="project2">
+                            <div class="image1">
+                                <img src="./images/dam.jpeg" alt="">
+                                <p>Construction and Development</p>
+                            </div>
+                            <div class="image2">
+                                <img src="./images/road.jpg" alt="">
+                                Roads
+                            </div>
+                            <div class="image3">
+                                <img src="./images/bases.jpeg" alt="">
+                                seaports </div>
+                        </div>
+                    </div>
+                    <div class="r5">
+                        <h3>Videos</h3>
+                        <div class="video1">
+                            <video width="220" height="240" autoplay>
+                                <source src="./images/abiy.mp4" type="video/mp4">
+                                <source src="movie.ogg" type="video/ogg">
+                                Your browser does not support the video tag.
+                              </video>
+                        </div>
+                        <div class="video2">
+                            <video width="220" height="240" autoplay>
+                                <source src="./images/242360139_533293514437620_3857052236060285755_n.mp4" type="video/mp4">
+                                <source src="movie.ogg" type="video/ogg">
+                                Your browser does not support the video tag.
+                              </video>
+                        </div>
+                        <div class="video3">
+                            <video width="220" height="240" autoplay>
+                                <source src="./images/army.mp4" type="video/mp4">
+                                <source src="movie.ogg" type="video/ogg">
+                                Your browser does not support the video tag.
+                              </video>
+                        </div>
+                    </div>
+                    <a class="see-more" href="#"> See More</a>
+
+                    <div class="r6">
+                        <!-- <h3>Related Links</h3> -->
+
+                        <div class="resource">
+
+                            <!-- <iframe src="./images/iframe.pdf" frameborder="0"></iframe> -->
+
+                        </div>
+
+
+                    </div>
+                    <div class="r7">
+                        <h3>Photo Albums</h3>
+                        <div class="album">
+                            <div class="album-content1">
+                                <img src="./images//images (2).jpg" alt="">
+                            </div>
+                            <div class="album-content2">
+                                <img src="./images/images (3).jpg" alt="">
+                            </div>
+                            <div class="album-content3"><img src="./images/images (4).jpg" alt=""></div>
+                            <!-- <div class="album-content4"><img src="./images/images (5).jpg" alt=""></div> -->
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col2">
+                <h3>Videos</h3>
+                    <div class="video-vertical">
+                        <div class="vv1">
+                        <video width="344.67" height="240" autoplay>
+                                <source src="./images/abiy.mp4" type="video/mp4">
+                                <source src="movie.ogg" type="video/ogg">
+                                Your browser does not support the video tag.
+                              </video>
+                        </div>
+                        <div class="vv2">
+                        <video width="344.67"" height="240" autoplay>
+                                <source src="./images/242360139_533293514437620_3857052236060285755_n.mp4" type="video/mp4">
+                                <source src="movie.ogg" type="video/ogg">
+                                Your browser does not support the video tag.
+                              </video>
+                        </div>
+                        <div class="vv3">
+                        <video width="344.67" height="240" autoplay>
+                                <source src="./images/army.mp4" type="video/mp4">
+                                <source src="movie.ogg" type="video/ogg">
+                                Your browser does not support the video tag.
+                              </video>
+                        </div>
+                    </div>
+                    <div class="ref-link">
+                        
+                    </div>
+                    
+                </div>
+            </div>
+
+        </div>
+    </div>
+    </div>
+    <?php include("footer.php"); ?>   
+
+
+    
+    
